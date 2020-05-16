ea823fe6a41f0a9e3a9fe7c2f268b6c9be95537c
MaurickThom <thomtwd@gmail.com>
2020-05-16 12:49:27 -0500

    first commit

---
+++ 1.cpp
@@ -0,0 +1,89 @@
+#include <windows.h>
+#include <GL/glut.h>
+#include <stdio.h>
+#include <iostream>
+
+void init()
+{
+   glMatrixMode(GL_PROJECTION);
+   gluOrtho2D(-5, 5, -5, 5);
+   glClearColor(0.0, 0.0, 0.0, 0.0);
+}
+void triangle()
+{
+   glClear(GL_COLOR_BUFFER_BIT);
+   glColor3f(0.431, 0.141, 0.706);
+   glBegin(GL_TRIANGLES);
+   glVertex3f(1, 1, 0.0);
+   glVertex3f(1, 0.00, 0.0);
+   glVertex3f(-1, 1, 0.0);
+   glEnd();
+   glFlush();
+}
+void point()
+{
+   glClear(GL_COLOR_BUFFER_BIT);
+   glColor3f(0.431, 0.141, 0.706);
+   glPointSize(10.0);
+   glBegin(GL_POINTS);
+   glVertex3f(0, 0, 0);
+   glEnd();
+   glFlush();
+}
+void line()
+{
+   glClear(GL_COLOR_BUFFER_BIT);
+   glColor3f(0.431, 0.141, 0.706);
+   glLineWidth(10.0f);
+   glBegin(GL_LINES);
+   glVertex3f(1, 1, 0.0);
+   glVertex3f(4, 4, 0.0);
+   glEnd();
+   glFlush();
+}
+
+void menu()
+{
+   int option = 0;
+   while (true)
+   {
+      std::cout << "1. Line" << std::endl;
+      std::cout << "2. Triangle" << std::endl;
+      std::cout << "3. Point" << std::endl;
+      std::cout << "4. Salir" << std::endl;
+      std::cout << "Option : ";
+      std::cin >> option;
+      if (option == 4)
+         return;
+      switch (option)
+      {
+      case 1:
+         glutDisplayFunc(line);
+         glutMainLoop();
+         break;
+      case 2:
+         glutDisplayFunc(triangle);
+         glutMainLoop();
+         break;
+      case 3:
+         glutDisplayFunc(point);
+         glutMainLoop();
+         // glutDestroyWindow(1);
+         break;
+      }
+   }
+}
+
+int main(int argc, char **argv)
+{
+   glutInit(&argc, argv);
+   glutCreateWindow("Menu");
+   glutInitWindowSize(600, 450);
+   glutInitWindowPosition(50, 50);
+   glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+   init();
+   menu();
+   return 0;
+}
+// g++ -m32 -Wall -o menu.out menu.cpp -L"C:\MinGW\lib" -lglu32 -lglut32 -lopengl32 -lstdc++
+// ./menu.out
---
+++ 2.cpp
@@ -0,0 +1,82 @@
+#include <GL/glut.h>
+void init (void)
+{
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+}
+/*
+void triangulo(){
+	glBegin (GL_TRIANGLES);
+	glVertex3f(0.25,0.25,0.0);
+	glVertex3f(0.25,0.00,0.25);
+	glVertex3f(-0.25,0.25,0.0);
+	glEnd();
+}*/
+
+void display(void)
+{
+	
+
+
+	glClear (GL_COLOR_BUFFER_BIT);
+	//head
+	glColor3f (1.0, 1.0, 1.0);
+	glLineWidth(5.0f);
+	glBegin(GL_LINES);
+	
+	glVertex2f (-0.3, -0.3);
+	glVertex2f (0.0, 0.1);
+	
+	glVertex2f (0.3, -0.3);
+	glVertex2f (0.0, 0.1);
+	
+	glVertex2f (-0.3, -0.3);
+	glVertex2f (0.3, -0.3);
+	
+	
+	glVertex2f (-0.2, -0.3);
+	glVertex2f (-0.2, -0.7);
+	
+	glVertex2f (0.2, -0.3);
+	glVertex2f (0.2, -.7);
+	
+	glVertex2f (-0.2, -0.7);
+	glVertex2f (0.2, -.7);
+	
+	glVertex2f (0.2, -.6);
+	glVertex2f (0.5, -.6);
+	
+	glVertex2f (-0.2, -.6);
+	glVertex2f (-.5, -.6);
+	
+	glVertex2f (.08, -.7);
+	glVertex2f (.08, -.45);
+
+	glVertex2f (-.08, -.7);
+	glVertex2f (-.08, -.45);
+	
+	glVertex2f (.08, -.45);
+	glVertex2f (-.08, -.45);
+	
+	glEnd();
+	
+	glBegin (GL_POINTS);
+	glVertex3f(5,5,5);
+	glEnd();
+
+	glFlush ();
+}
+int main(int argc, char** argv)
+{
+	glutInit(&argc, argv);
+	glutInitWindowSize (600, 500);
+	glutInitWindowPosition (200, 100);
+	glutCreateWindow ("My House");
+	init();
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
+
