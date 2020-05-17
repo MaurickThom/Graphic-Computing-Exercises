0b243c366b309ec95a5029881acf741e66a8d4d9
MaurickThom <thomtwd@gmail.com>
2020-05-17 15:53:02 -0500

    3

--- 3.cpp
+++ 3.cpp
@@ -10,55 +10,90 @@ void init()
     gluOrtho2D(-5, 5, -5, 5);
     glClearColor(0.0, 0.0, 0.0, 0.0);
 }
+void ReProyectar(int w, int h)
+{
+    GLfloat formato;
 
+    if (h == 0)
+        h = 1;
+
+    glViewport(0, 0, w, h);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    formato = (GLfloat)w / (GLfloat)h;
+    if (w <= h)
+        glOrtho(-10.0f, 10.0f, -10.0f / formato, 10.0f / formato, -1.0f, 1.0f);
+    else
+        glOrtho(-10.0f * formato, 10.0f * formato, -10.0f, 10.0f, -1.0f, 1.0f);
+}
 void drawScene()
 {
-    // glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_MODELVIEW);
     glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
     glColor3f(0.431, 0.141, 0.706);
     glColor3f(1, 1, 1);
-    glBegin(GL_LINE_STRIP);
-    glVertex3i(1, 2, 0);
-    glVertex3i(5, 2, 0);
-    glVertex3i(5, 4, 0);
-    glVertex3i(3, 4, 0);
-    glVertex3i(3, 5, 0);
-    glVertex3i(4, 6, 0);
-    glVertex3i(6, 6, 0);
-    glVertex3i(6, 2, 0);
-    glVertex3i(9, 2, 0);
-    glVertex3i(9, 4, 0);
-    glVertex3i(8, 4, 0);
-    glVertex3i(8, 7, 0);
-    glVertex3i(9, 6, 0);
-    glVertex3i(11, 5, 0);
-    glVertex3i(13, 5, 0);
-    glVertex3i(15, 6, 0);
-    glVertex3i(16, 7, 0);
-    glVertex3i(16, 13, 0);
-    glVertex3i(14, 11, 0);
-    glVertex3i(10, 11, 0);
-    glVertex3i(8, 13, 0);
-    glVertex3i(8, 9, 0);
-    glVertex3i(3, 11, 0);
-    glVertex3i(4, 11, 0);
-    glVertex3i(2, 9, 0);
-    glVertex3i(2, 12, 0);
-    glVertex3i(3, 12, 0);
-    glVertex3i(5, 14, 0);
-    glVertex3i(5, 15, 0);
-    glVertex3i(3, 15, 0);
-    glVertex3i(0, 12, 0);
-    glVertex3i(0, 8, 0);
-    glVertex3i(1, 2, 0);
+   glBegin(GL_LINE_LOOP);
+    //glBegin(GL_LINE_STRIP);
+    glVertex3i(1-10, 2-10, 0);
+    glVertex3i(5-10, 2-10, 0);
+    
+    glVertex3i(5-10, 4-10, 0);
+    glVertex3i(3-10, 4-10, 0);
+    glVertex3i(3-10, 5-10, 0);
+    glVertex3i(4-10, 6-10, 0);
+    glVertex3i(6-10, 6-10, 0);
+    glVertex3i(6-10, 2-10, 0);
+    glVertex3i(9-10, 2-10, 0);
+    glVertex3i(9-10, 4-10, 0);
+    glVertex3i(8-10, 4-10, 0);
+    glVertex3i(8-10, 7-10, 0);
+    glVertex3i(9-10, 6-10, 0);
+    glVertex3i(11-10, 5-10, 0);
+    glVertex3i(13-10, 5-10, 0);
+    glVertex3i(15-10, 6-10, 0);
+    glVertex3i(16-10, 7-10, 0);
+    glVertex3i(16-10, 13-10, 0);
+    glVertex3i(14-10, 11-10, 0);
+    glVertex3i(10-10, 11-10, 0);
+    glVertex3i(8-10, 13-10, 0);
+    glVertex3i(8-10, 9-10, 0);
+    glVertex3i(6-10, 11-10, 0);
+    glVertex3i(4-10, 11-10, 0);
+    glVertex3i(2-10, 9-10, 0);
+    glVertex3i(2-10, 12-10, 0);
+    glVertex3i(3-10, 12-10, 0);
+	glVertex3i(5-10, 14-10, 0);
+	glVertex3i(5-10, 15-10, 0);
+    glVertex3i(3-10, 15-10, 0);
+    glVertex3i(0-10, 12-10, 0);
+	glVertex3i(0-10, 8-10, 0);
+	//glVertex3i(1-10, 2-10, 0);//GL_LINE_STRIP
+	
     glEnd();
-
-    // glBegin(GL_LINE_LOOP);
-    //     glVertex3i(-3, 4, 0);
-    //     glVertex3i(3, 4, 0);
-    //     glVertex3i(3, -6, 0);
-    //     glVertex3i(-3, -6, 0);
-    // glEnd();
+    
+    glBegin(GL_LINE_STRIP);
+    glVertex3i(9-10, 8-10, 0);
+	glVertex3i(11-10, 8-10, 0);
+	glVertex3i(11-10, 10-10, 0);
+	glVertex3i(9-10, 10-10, 0);
+	glVertex3i(9-10, 8-10, 0);
+    glEnd();
+    
+    glBegin(GL_LINE_LOOP);
+    glVertex3i(13-10, 8-10, 0);
+	glVertex3i(15-10, 8-10, 0);
+	glVertex3i(15-10, 10-10, 0);
+	glVertex3i(13-10, 10-10, 0);
+    glEnd();
+    
+    glBegin(GL_LINE_LOOP);
+    glVertex3i(11-10, 7-10, 0);
+	glVertex3i(13-10, 7-10, 0);
+	glVertex3i(12-10, 8-10, 0);
+    glEnd();
     glFlush();
 }
 
@@ -70,7 +105,7 @@ int main(int argc, char **argv)
     glutInitWindowPosition(800, 800);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
     init();
-    // glutReshapeFunc(ReProyectar);
+    glutReshapeFunc(ReProyectar);
     glutDisplayFunc(drawScene);
     // glutIdleFunc(drawScene);
     glutMainLoop();
